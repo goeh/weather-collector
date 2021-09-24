fb13b13881e3525b22649721ca29198d3b826aa7
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-09-24 17:24:42 +0200

    updated build.gradle and wrote mock producer

--- src/main/java/nl/tudelft/streaming/DavisProducer.java
+++ src/main/java/nl/tudelft/streaming/DavisProducer.java
@@ -1,4 +1,37 @@
 package nl.tudelft.streaming;
 
+import org.apache.pulsar.client.api.*;
+import java.io.IOException;
+
 public class DavisProducer {
+
+    private static final String SERVICE_URL = "someserviceurl";
+
+    public static void main(String[] args) throws IOException
+    {
+
+        // Create client object
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl(SERVICE_URL)
+                .authentication(
+                        AuthenticationFactory.token("verylongtokenhere")
+                )
+                .build();
+
+        // Create producer on a topic
+        Producer<byte[]> producer = client.newProducer()
+                .topic("sometopichere")
+                .create();
+
+        // Send a message to the topic
+        producer.send("Hey hey this is a message!\n".getBytes());
+
+        //Close the producer
+        producer.close();
+
+        // Close the client
+        client.close();
+
+    }
+
 }

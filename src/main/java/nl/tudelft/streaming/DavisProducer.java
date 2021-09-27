71792edb225d8ecfc7e08b058be2ae7ff79a4229
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-09-27 17:09:36 +0200

    added copyright and author

--- src/main/java/nl/tudelft/streaming/DavisProducer.java
+++ src/main/java/nl/tudelft/streaming/DavisProducer.java
@@ -1,8 +1,28 @@
+/*
+ *  Copyright 2021 Saverio Guzzo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
 package nl.tudelft.streaming;
 
 import org.apache.pulsar.client.api.*;
 import java.io.IOException;
 
+/**
+ *
+ * @author Saverio Guzzo <saverio.g.guzzo@gmail.com>
+ */
 public class DavisProducer {
 
     private static final String SERVICE_URL = "someserviceurl";
